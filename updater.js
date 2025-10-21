7572f23186fb914cbcf0b46164f5993637528403
Ivaylo Zdravkov <133565939+ivajlozdr@users.noreply.github.com>
2025-10-21 13:49:32 +0300

    actually automate age (and fix cron)

--- updater.js
+++ updater.js
@@ -8,18 +8,50 @@ const now = new Date();
 
 let years = now.getFullYear() - birth.getFullYear();
 let months = now.getMonth() - birth.getMonth();
+let days = now.getDate() - birth.getDate();
+
+if (days < 0) {
+    months--;
+    const prevMonth = new Date(now.getFullYear(), now.getMonth(), 0);
+    days += prevMonth.getDate();
+}
+
 if (months < 0) {
     years--;
     months += 12;
 }
 
-const uptimeText = `${years} years, ${months} months`;
-console.log("New uptime:", uptimeText);
+let dots = " ....................... ";
 
-let dots = " ............................... ";
-if (months >= 10) {
-    dots = dots.slice(0, -2) + " "; // yeet
+let dotAdjustment = 0;
+
+if (months >= 10) dotAdjustment--;
+if (days >= 10) dotAdjustment--;
+
+let monthText = "months";
+if (months === 1) {
+    monthText = "month";
+    dotAdjustment++;
 }
+
+let dayText = "days";
+if (days === 1) {
+    dayText = "day";
+    dotAdjustment++;
+}
+
+let interiorDots = dots.trim();
+
+if (dotAdjustment < 0) {
+    interiorDots = interiorDots.slice(0, dotAdjustment);
+} else if (dotAdjustment > 0) {
+    interiorDots += ".".repeat(dotAdjustment);
+}
+
+dots = ` ${interiorDots} `;
+
+const uptimeText = `${years} years, ${months} ${monthText}, ${days} ${dayText}`;
+console.log("New uptime:", uptimeText);
 
 svg = svg.replace(
     /(<tspan[^>]*id="age_data"[^>]*>)([^<]+)(<\/tspan>)/,
@@ -30,6 +62,5 @@ svg = svg.replace(
     /(<tspan[^>]*id="age_data_dots"[^>]*>)([^<]+)(<\/tspan>)/,
     `$1${dots}$3`
 );
-
 
 fs.writeFileSync(svgPath, svg);
